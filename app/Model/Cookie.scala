19d9326048d8c450309b6e08026c7a208d26cbb2
norang <noranglemon@gmail.com>
2018-02-22 11:09:04 +0900

    remove 'var' to make them immutable

--- app/Model/Cookie.scala
+++ app/Model/Cookie.scala
@@ -1,6 +1,6 @@
 package Model
 
-case class Cookie(val name: String, var level: Int, var inventory: Inventory, var wallet: Wallet)
+case class Cookie(name: String, level: Int, inventory: Inventory, wallet: Wallet)
 
 case class Inventory(jelly: Int, chocolate: Int, cream: Int)
 
